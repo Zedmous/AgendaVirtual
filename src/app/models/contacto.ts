285d5ee3d960c582cd6f8864506dcdf55d1edf76
Zedmous <ejnsilva29@gmail.com>
2021-05-01 05:49:21 -0400

    Vistas dseñadas, botones del contacto

---
+++ src/app/models/contacto.ts
@@ -0,0 +1,11 @@
+export class Contacto{
+    _id?:number;
+    nombre:string;
+    apellido:string;
+    telefono:string;
+    constructor(nombre:string,apellido:string,telefono:string){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.telefono=telefono;
+    }
+}
